e6e181dd3b843903c5825be02524ee05a2824a92
Zhangyifan0016 <z351072259@163.com>
2022-07-14 22:49:21 +0800

    feat(Breadcrumb): 修改面包屑功能

--- src/components/Breadcrumb.vue
+++ src/components/Breadcrumb.vue
@@ -1,16 +1,20 @@
 <template>
   <div class="main">
-    <el-breadcrumb
-      v-if="this.$route.path !== '/'"
-      separator-class="el-icon-arrow-right"
-    >
-      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-      <el-breadcrumb-item
-        :to="{ path: '/sys/users' }"
-        v-for="(item, index) in bread"
-        :key="index"
-        >{{ item.meta.title }}</el-breadcrumb-item
-      >
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <template v-if="$route.path === '/'">
+        <el-breadcrumb-item>
+          <span class="active">控制台</span>
+        </el-breadcrumb-item>
+      </template>
+      <template v-else>
+        <el-breadcrumb-item
+          :to="index <= breadcrumbList.length - 1 ? item.path : ''"
+          v-for="(item, index) in breadcrumbList"
+          :key="index"
+        >
+          {{ item.title }}
+        </el-breadcrumb-item>
+      </template>
     </el-breadcrumb>
   </div>
 </template>
@@ -18,14 +22,41 @@
 export default {
   name: 'Breadcrumb',
   data() {
-    return {}
+    return {
+      breadcrumbList: []
+    }
   },
-  methods: {},
-  computed: {
-    bread() {
-      return this.$route.matched
+  watch: {
+    $route: {
+      handler(newVal, oldVal) {
+        this.handleGetBreadcrumbList()
+      },
+      immediate: true,
+      deep: true
+    }
+  },
+  methods: {
+    handleGetBreadcrumbList() {
+      const breadcrumbList = [{ title: '首页', path: '/' }]
+      const routeList = this.$route.matched
+      routeList.forEach((item) => {
+        if (item.meta.title) {
+          breadcrumbList.push({
+            title: item.meta.title,
+            path: item.path
+          })
+        }
+      })
+      console.log(breadcrumbList)
+      this.breadcrumbList = breadcrumbList
     }
   }
 }
 </script>
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.active {
+  font-weight: bold;
+  font-size: 16px;
+  color: #000;
+}
+</style>
